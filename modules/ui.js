84862211b48680e34839838dafab8b1495bc59fc
Шохрух <138532283+Shoxrux469@users.noreply.github.com>
2023-11-24 12:36:24 +0500

    fixed

--- modules/ui.js
+++ modules/ui.js
@@ -1,7 +1,7 @@
 import { getData } from "./https";
-
-let body = document.body
-let pop_movies_box = document.querySelector('.pop_movies_box')
+import axios from "axios";
+let body = document.body;
+let pop_movies_box = document.querySelector(".pop_movies_box");
 
 export function header() {
   let header = document.querySelector("header");
@@ -31,55 +31,54 @@ export function header() {
     <button class="sign_in">Войти</button>
   </div>
 </div>
-  `
-
+  `;
 }
 
-
-
-getData('/movie/now_playing?page=2&language=ru')
-  .then(res => {
-    reload(res.data.results, pop_movies_box)
-    console.log(res.data.results);
-  })
-
+getData("/movie/now_playing?page=2&language=ru").then((res) => {
+  reload(res.data.results, pop_movies_box);
+  console.log(res.data.results);
+});
 
 function reload(arr, place) {
-    place.innerHTML = ''
-  
-    for(let item of arr) {
-      
-      let img_box = document.createElement('div')
-      let img_bg = document.createElement('div')
-      let img_bg_btn = document.createElement('button')
-      let img = document.createElement('img')
-	  let title = document.createElement('p')
-	  let release_date = document.createElement('span')
-	  let img_rating = document.createElement('div')
+  place.innerHTML = "";
 
-      img_box.classList.add('now_playing_movies_imgs')
-      img_bg.classList.add('now_playing_movies_bg')
-	  img_rating.classList.add('now_playing_movies_rating')
-      img_bg_btn.classList.add('now_playing_movies_bg_btn')
-	  title.classList.add('now_playing_movies_title')
-	  release_date.classList.add('now_playing_movies_rel_date')
+  for (let item of arr) {
+    let img_box = document.createElement("div");
+    let img_bg = document.createElement("div");
+    let img_bg_btn = document.createElement("button");
+    let img = document.createElement("img");
+    let title = document.createElement("p");
+    let release_date = document.createElement("span");
+    let img_rating = document.createElement("div");
 
+    img_box.classList.add("now_playing_movies_imgs");
+    img_bg.classList.add("now_playing_movies_bg");
+    img_rating.classList.add("now_playing_movies_rating");
+    img_bg_btn.classList.add("now_playing_movies_bg_btn");
+    title.classList.add("now_playing_movies_title");
+    release_date.classList.add("now_playing_movies_rel_date");
 
-      img_bg_btn.innerHTML = "Карточка фильма"
-	  img.src = "https://image.tmdb.org/t/p/original" +  item.poster_path
-	  title.innerHTML = item.original_title
-	  img_rating.innerHTML = item.vote_average.toFixed(2)
-	  release_date.innerHTML = item.release_date
-  
-      img_bg.onmouseover = () => {
-        img_bg_btn.classList.add('show')
-      }
-      img_bg.onmouseout = () => {
-        img_bg_btn.classList.remove('show')
-      }
-      place.append(img_box)
-      img_box.append(img_bg, img, title, release_date)
-      img_bg.append(img_bg_btn, img_rating)
+    img_bg_btn.innerHTML = "Карточка фильма";
+    img.src = "https://image.tmdb.org/t/p/original" + item.poster_path;
+    title.innerHTML = item.original_title;
+    img_rating.innerHTML = item.vote_average.toFixed(2);
+    release_date.innerHTML = item.release_date;
 
-    }
+    img_bg.onmouseover = () => {
+      img_bg_btn.classList.add("show");
+    };
+    img_bg.onmouseout = () => {
+      img_bg_btn.classList.remove("show");
+    };
+    img_bg_btn.onclick = () => {
+      getData(`movie/${ item.id }/videos`)
+	  	.then((res) => {
+        console.log(res);
+      });
+    };
+
+    place.append(img_box);
+    img_box.append(img_bg, img, title, release_date);
+    img_bg.append(img_bg_btn, img_rating);
   }
+}
