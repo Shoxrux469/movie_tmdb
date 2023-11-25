13e7b48d329bf09c1663feb9f6c017326f45d037
Шохрух <138532283+Shoxrux469@users.noreply.github.com>
2023-11-25 18:14:13 +0500

    fixed

--- main.js
+++ main.js
@@ -1,4 +1,27 @@
-import { header } from './modules/ui.js'
+import { header, reload_pop_movies, reload_now_playing } from "./modules/ui.js";
+import { getData } from "./modules/https.js";
+let iframe = document.querySelector("iframe");
+let pop_movies_box = document.querySelector(".pop_movies_box");
+let swiper_wrapper = document.querySelector(".swiper-wrapper");
+let pop_stars_content = document.querySelector(".pop_stars_content");
 
-header()
-console.log(header());
+
+header();
+
+export function setTrailer(video) {
+  iframe.src = "https://www.youtube.com/watch?v=" + video.key;
+}
+
+getData('/person/popular')
+  .then(res => {
+    reload_pop_stars(res.data.results, pop_stars_content)
+  })
+
+Promise.all([
+  getData("/movie/now_playing?page=2&language=ru"),
+  getData("/movie/popular"),
+  getData("/genre/movie/list"),
+]).then(([movies_now_playing,movies_popular,  genres]) => {
+  reload_now_playing(movies_now_playing.data.results, pop_movies_box, genres.data.genres);
+  reload_pop_movies(movies_popular.data.results, swiper_wrapper, genres.data.genres);
+});
--- modules/ui.js
+++ modules/ui.js
@@ -1,45 +1,42 @@
+import { setTrailer } from "../main";
 import { getData } from "./https";
-import axios from "axios";
-let body = document.body;
-let pop_movies_box = document.querySelector(".pop_movies_box");
-
+import Swiper from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+let swiper_pagination = document.querySelector('.swiper-pagination')
 export function header() {
   let header = document.querySelector("header");
 
   header.innerHTML = `
   <div class="header">
   <div class="left">
-    <div class="logo">
-      <img src="./public/cinema_logo.svg" alt="">
-      <p> <span>Kino</span>area</p>
-    </div>
-    <div class="networks">
-      <a href="#"><img src="./public/networks_immg.svg" alt=""></a>
+  <div class="logo">
+  <img src="./public/cinema_logo.svg" alt="">
+  <p> <span>Kino</span>area</p>
+  </div>
+  <div class="networks">
+  <a href="#"><img src="./public/networks_immg.svg" alt=""></a>
     </div>
   </div>
   <nav class="center">
-    <a href="#">Афиша</a>
-    <a href="#">Медиа</a>
-    <a href="#">Фильмы</a>
-    <a href="#">Актёры</a>
-    <a href="#">Новости</a>
-    <a href="#">Подборки</a>
+  <a href="#">Афиша</a>
+  <a href="#">Медиа</a>
+  <a href="#">Фильмы</a>
+  <a href="#">Актёры</a>
+  <a href="#">Новости</a>
+  <a href="#">Подборки</a>
     <a href="#">Категории</a>
-  </nav>
-  <div class="right">
+    </nav>
+    <div class="right">
     <button class="searcher_btn"><img src="./public/searcher_btn.svg" alt=""></button>
     <button class="sign_in">Войти</button>
-  </div>
-</div>
-  `;
+    </div>
+    </div>
+    `;
 }
 
-getData("/movie/now_playing?page=2&language=ru").then((res) => {
-  reload(res.data.results, pop_movies_box);
-  console.log(res.data.results);
-});
-
-function reload(arr, place) {
+export function reload_now_playing(arr, place, genres) {
   place.innerHTML = "";
 
   for (let item of arr) {
@@ -48,21 +45,16 @@ function reload(arr, place) {
     let img_bg_btn = document.createElement("button");
     let img = document.createElement("img");
     let title = document.createElement("p");
-    let release_date = document.createElement("span");
+    let genre_span = document.createElement("span");
     let img_rating = document.createElement("div");
+    let genreTitles = [];
 
-    img_box.classList.add("now_playing_movies_imgs");
-    img_bg.classList.add("now_playing_movies_bg");
-    img_rating.classList.add("now_playing_movies_rating");
-    img_bg_btn.classList.add("now_playing_movies_bg_btn");
-    title.classList.add("now_playing_movies_title");
-    release_date.classList.add("now_playing_movies_rel_date");
-
-    img_bg_btn.innerHTML = "Карточка фильма";
-    img.src = "https://image.tmdb.org/t/p/original" + item.poster_path;
-    title.innerHTML = item.original_title;
-    img_rating.innerHTML = item.vote_average.toFixed(2);
-    release_date.innerHTML = item.release_date;
+    img_box.classList.add("movies_imgs");
+    img_bg.classList.add("movies_bg");
+    img_rating.classList.add("movies_rating");
+    img_bg_btn.classList.add("movies_bg_btn");
+    title.classList.add("movies_title");
+    genre_span.classList.add("movies_genre");
 
     img_bg.onmouseover = () => {
       img_bg_btn.classList.add("show");
@@ -71,14 +63,156 @@ function reload(arr, place) {
       img_bg_btn.classList.remove("show");
     };
     img_bg_btn.onclick = () => {
-      getData(`movie/${ item.id }/videos`)
-	  	.then((res) => {
+      getData(`movie/${item.id}/videos`).then((res) => {
         console.log(res);
+        setTrailer(res.data.results[0]);
       });
     };
+    for (let id of item.genre_ids) {
+      for (let genre of genres) {
+        if (id == genre.id) {
+          genreTitles.push(genre.name);
+        }
+      }
+    }
+
+    img_bg_btn.innerHTML = "Карточка фильма";
+    img.src = "https://image.tmdb.org/t/p/original" + item.poster_path;
+    title.innerHTML = item.original_title;
+    img_rating.innerHTML = item.vote_average.toFixed(2);
+    genre_span.innerHTML = genreTitles.join(", ");
 
     place.append(img_box);
-    img_box.append(img_bg, img, title, release_date);
+    img_box.append(img_bg, img, title, genre_span);
     img_bg.append(img_bg_btn, img_rating);
   }
+}
+
+export function reload_pop_movies(arr, place, genres) {
+  place.innerHTML = "";
+
+  for (let item of arr) {
+    let img_box = document.createElement("div");
+    let img_bg = document.createElement("div");
+    let img_rating = document.createElement("div");
+    let img_bg_btn = document.createElement("button");
+    let img = document.createElement("img");
+    let genre_span = document.createElement("span");
+    let title = document.createElement("p");
+    let genreTitles = [];
+
+    img_box.classList.add("movies_imgs");
+    img_box.classList.add("swiper-slide");
+    img_bg.classList.add("movies_bg");
+    img_rating.classList.add("movies_rating");
+    img_bg_btn.classList.add("movies_bg_btn");
+    title.classList.add("movies_title");
+    genre_span.classList.add("movies_genre");
+    img.src = "https://image.tmdb.org/t/p/original" + item.poster_path;
+	swiper_pagination.innerHTML = "4/" + arr.length;
+	// console.log(arr.length);
+
+    img_bg.onmouseover = () => {
+      img_bg_btn.classList.add("show");
+    };
+    img_bg.onmouseout = () => {
+      img_bg_btn.classList.remove("show");
+    };
+    img_bg_btn.onclick = () => {
+      getData(`movie/${item.id}/videos`).then((res) => {
+        console.log(res);
+        setTrailer(res.data.results[0]);
+      });
+    };
+    for (let id of item.genre_ids) {
+      for (let genre of genres) {
+        if (id == genre.id) {
+          genreTitles.push(genre.name);
+        }
+      }
+    }
+
+    const swiper = new Swiper(".swiper", {
+      loop: true,
+
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      Pagination: {
+        bulletActiveClass: "swiper-pagination-bullet-active",
+        bulletClass: "swiper-pagination-bullet",
+        bulletElement: "span",
+        clickable: true,
+        clickableClass: "swiper-pagination-clickable",
+        currentClass: "swiper-pagination-current",
+        dynamicBullets: false,
+        dynamicMainBullets: 1,
+        hiddenClass: "swiper-pagination-hidden",
+        hideOnClick: true,
+        horizontalClass: "swiper-pagination-horizontal",
+        lockClass: "swiper-pagination-lock",
+        modifierClass: "swiper-pagination-",
+        paginationDisabledClass: "swiper-pagination-disabled",
+        progressbarFillClass: "swiper-pagination-progressbar-fill",
+        progressbarOpposite: false,
+		el: '.swiper-pagination',
+        progressbarOppositeClass: "swiper-pagination-progressbar-opposite",
+        totalClass: "swiper-pagination-total",
+        type: "bullets",
+        verticalClass: "swiper-pagination-vertical",
+      },
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + (index + 1) + "</span>";
+      },
+      renderCustom: function (swiper, current, total) {
+        return current + " of " + total;
+      },
+      renderFraction: function (currentClass, totalClass) {
+        return (
+          '<span class="' +
+          currentClass +
+          '"></span>' +
+          " of " +
+          '<span class="' +
+          totalClass +
+          '"></span>'
+        );
+      },
+      renderProgressbar: function (progressbarFillClass) {
+        return '<span class="' + progressbarFillClass + '"></span>';
+      },
+      autoplay: {
+        delay: 5000,
+      },
+
+      scrollbar: {
+        el: ".swiper-scrollbar",
+      },
+    });
+
+    img_bg_btn.innerHTML = "Карточка фильма";
+    img.src = "https://image.tmdb.org/t/p/original" + item.poster_path;
+    title.innerHTML = item.original_title;
+    img_rating.innerHTML = item.vote_average.toFixed(2);
+    genre_span.innerHTML = genreTitles.join(", ");
+
+    place.append(img_box);
+    img_box.append(img_bg, img, title, genre_span);
+    img_bg.append(img_bg_btn, img_rating);
+  }
+}
+
+export function reload_pop_stars(arr, place) {
+	place.innerHTML = ""
+	for(let item of arr) {
+		let img_box = document.createElement('div')
+		let img = document.createElement('img')
+		let img_rating = document.createElement('span')
+		let img_title = document.createElement('h2')
+		let origin_title = document.createElement('p')
+		let life_span = document.createElement('span')
+
+		
+	}
 }
