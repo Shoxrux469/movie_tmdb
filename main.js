8b64d9f2415f09b01b70d42a2aad23511dd7bc36
Шохрух <138532283+Shoxrux469@users.noreply.github.com>
2023-11-27 15:17:20 +0500

    fixed

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
-import { header, reload_pop_movies, reload_now_playing, reload_pop_stars } from "./modules/ui.js";
+import {
+  header,
+  reload_pop_movies,
+  reload_now_playing,
+  reload_pop_stars,
+  reload_upcoming,
+  reload_top_rated,
+  reload_pop_stars_list,
+} from "./modules/ui.js";
 import { getData } from "./modules/https.js";
 let iframe = document.querySelector("iframe");
 let pop_movies_box = document.querySelector(".pop_movies_box");
 let swiper_wrapper = document.querySelector(".swiper-wrapper");
 let pop_stars_content = document.querySelector(".pop_stars_content");
-
+let pop_stars_list = document.querySelector(".pop_stars_list");
+let upcoming_box = document.querySelector(".upcoming_content");
+let top_rated_content = document.querySelector(".top_rated_content");
 
 header();
 
@@ -12,16 +22,31 @@ export function setTrailer(video) {
   iframe.src = "https://www.youtube.com/watch?v=" + video.key;
 }
 
-getData('/person/popular')
-  .then(res => {
-    reload_pop_stars(res.data.results, pop_stars_content)
-  })
+getData("/person/popular").then((res) => {
+  reload_pop_stars(res.data.results.slice(0, 2), pop_stars_content),
+    reload_pop_stars_list(res.data.results, pop_stars_list);
+});
+
+getData("/movie/upcoming").then((res) =>
+  reload_upcoming(res.data.results, upcoming_box)
+);
+getData("/movie/top_rated").then((res) =>
+  reload_top_rated(res.data.results.slice(0, 5), top_rated_content)
+);
 
 Promise.all([
   getData("/movie/now_playing?page=2&language=ru"),
   getData("/movie/popular"),
   getData("/genre/movie/list"),
-]).then(([movies_now_playing,movies_popular,  genres]) => {
-  reload_now_playing(movies_now_playing.data.results, pop_movies_box, genres.data.genres);
-  reload_pop_movies(movies_popular.data.results, swiper_wrapper, genres.data.genres);
+]).then(([movies_now_playing, movies_popular, genres]) => {
+  reload_now_playing(
+    movies_now_playing.data.results.slice(0, 8),
+    pop_movies_box,
+    genres.data.genres
+  );
+  reload_pop_movies(
+    movies_popular.data.results,
+    swiper_wrapper,
+    genres.data.genres
+  );
 });
--- modules/ui.js
+++ modules/ui.js
@@ -4,7 +4,9 @@ import Swiper from "swiper";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-let swiper_pagination = document.querySelector('.swiper-pagination')
+let swiper_pagination = document.querySelector(".swiper-pagination");
+let body = document.body;
+let iframe = document.querySelector("iframe");
 export function header() {
   let header = document.querySelector("header");
 
@@ -63,9 +65,12 @@ export function reload_now_playing(arr, place, genres) {
       img_bg_btn.classList.remove("show");
     };
     img_bg_btn.onclick = () => {
-      getData(`movie/${item.id}/videos`).then((res) => {
-        console.log(res);
+      getData(`/movie/${item.id}/videos`).then((res) => {
         setTrailer(res.data.results[0]);
+      });
+
+      iframe.scrollIntoView({
+        behavior: "smooth",
       });
     };
     for (let id of item.genre_ids) {
@@ -93,9 +98,6 @@ export function reload_pop_movies(arr, place, genres) {
 
   for (let item of arr) {
     let img_box = document.createElement("div");
-    let img_bg = document.createElement("div");
-    let img_rating = document.createElement("div");
-    let img_bg_btn = document.createElement("button");
     let img = document.createElement("img");
     let genre_span = document.createElement("span");
     let title = document.createElement("p");
@@ -103,27 +105,11 @@ export function reload_pop_movies(arr, place, genres) {
 
     img_box.classList.add("movies_imgs");
     img_box.classList.add("swiper-slide");
-    img_bg.classList.add("movies_bg");
-    img_rating.classList.add("movies_rating");
-    img_bg_btn.classList.add("movies_bg_btn");
     title.classList.add("movies_title");
     genre_span.classList.add("movies_genre");
     img.src = "https://image.tmdb.org/t/p/original" + item.poster_path;
-	swiper_pagination.innerHTML = "4/" + arr.length;
-	// console.log(arr.length);
+    swiper_pagination.innerHTML = "4/" + arr.length;
 
-    img_bg.onmouseover = () => {
-      img_bg_btn.classList.add("show");
-    };
-    img_bg.onmouseout = () => {
-      img_bg_btn.classList.remove("show");
-    };
-    img_bg_btn.onclick = () => {
-      getData(`movie/${item.id}/videos`).then((res) => {
-        console.log(res);
-        setTrailer(res.data.results[0]);
-      });
-    };
     for (let id of item.genre_ids) {
       for (let genre of genres) {
         if (id == genre.id) {
@@ -133,104 +119,134 @@ export function reload_pop_movies(arr, place, genres) {
     }
 
     const swiper = new Swiper(".swiper", {
-      loop: true,
-
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-      Pagination: {
-        bulletActiveClass: "swiper-pagination-bullet-active",
-        bulletClass: "swiper-pagination-bullet",
-        bulletElement: "span",
-        clickable: true,
-        clickableClass: "swiper-pagination-clickable",
-        currentClass: "swiper-pagination-current",
-        dynamicBullets: false,
-        dynamicMainBullets: 1,
-        hiddenClass: "swiper-pagination-hidden",
-        hideOnClick: true,
-        horizontalClass: "swiper-pagination-horizontal",
-        lockClass: "swiper-pagination-lock",
-        modifierClass: "swiper-pagination-",
-        paginationDisabledClass: "swiper-pagination-disabled",
-        progressbarFillClass: "swiper-pagination-progressbar-fill",
-        progressbarOpposite: false,
-		el: '.swiper-pagination',
-        progressbarOppositeClass: "swiper-pagination-progressbar-opposite",
-        totalClass: "swiper-pagination-total",
-        type: "bullets",
-        verticalClass: "swiper-pagination-vertical",
-      },
-      renderBullet: function (index, className) {
-        return '<span class="' + className + '">' + (index + 1) + "</span>";
-      },
-      renderCustom: function (swiper, current, total) {
-        return current + " of " + total;
-      },
-      renderFraction: function (currentClass, totalClass) {
-        return (
-          '<span class="' +
-          currentClass +
-          '"></span>' +
-          " of " +
-          '<span class="' +
-          totalClass +
-          '"></span>'
-        );
-      },
-      renderProgressbar: function (progressbarFillClass) {
-        return '<span class="' + progressbarFillClass + '"></span>';
-      },
       autoplay: {
-        delay: 5000,
+        delay: 0,
+        pauseOnMouseEnter: true, // added
+        disableOnInteraction: false, // added
       },
+      speed: 3000,
+      slidesPerView: 3,
 
       scrollbar: {
         el: ".swiper-scrollbar",
       },
     });
 
-    img_bg_btn.innerHTML = "Карточка фильма";
     img.src = "https://image.tmdb.org/t/p/original" + item.poster_path;
     title.innerHTML = item.original_title;
-    img_rating.innerHTML = item.vote_average.toFixed(2);
     genre_span.innerHTML = genreTitles.join(", ");
 
     place.append(img_box);
-    img_box.append(img_bg, img, title, genre_span);
-    img_bg.append(img_bg_btn, img_rating);
+    img_box.append(img, title, genre_span);
   }
 }
 
 export function reload_pop_stars(arr, place) {
+  place.innerHTML = "";
+  for (let item of arr) {
+    let img_box = document.createElement("div");
+    let img = document.createElement("img");
+    let img_title = document.createElement("h2");
+    let origin_title = document.createElement("p");
+    let known_for = document.createElement("span");
+    let div = document.createElement("div");
+
+    img_box.classList.add("pop_stars_box");
+    img.classList.add("pop_stars_img");
+    div.classList.add("pop_stars_div");
+
+    img.src = "https://image.tmdb.org/t/p/original" + item.profile_path;
+    img_title.innerHTML = item.name;
+    origin_title.innerHTML = item.original_name;
+    div.append(img_title, origin_title, known_for);
+    img_box.append(img, div);
+    place.append(img_box);
+
+    for (let film of item.known_for) {
+      known_for.innerHTML += film.title + ", ";
+    }
+  }
+}
+
+export function reload_pop_stars_list(arr, place) {
+  place.innerHTML = "";
+  for (let item of arr) {
+    let info_box = document.createElement("div");
+    let info_box_left = document.createElement("div");
+    let info_box_right = document.createElement("span");
+    let info_box_title = document.createElement("h2");
+    let info_box_origin_title = document.createElement("p");
+
+    info_box.classList.add("info_box");
+    info_box_left.classList.add("info_box_left");
+    info_box_right.classList.add("info_box_right");
+
+    info_box_right.innerHTML = item.popularity.toFixed(0);
+    info_box_title.innerHTML = item.name;
+    info_box_origin_title.innerHTML = item.original_name;
+
+    place.append(info_box);
+    info_box.append(info_box_left, info_box_right);
+    info_box_left.append(info_box_title, info_box_origin_title);
+  }
+}
+export function reload_upcoming(arr, place) {
+  place.innerHTML = "";
+  for (let item of arr) {
+    let img_box = document.createElement("div");
+    let img = document.createElement("img");
+    let title = document.createElement("p");
+    let release_date = document.createElement("span");
+
+    img_box.classList.add("upcoming_item");
+    img_box.classList.add("swiper-wrapper");
+    img_box.classList.add("swiper-slide");
+
+    img.src = "https://image.tmdb.org/t/p/original" + item.poster_path;
+    title.innerHTML = item.title;
+    release_date.innerHTML = item.release_date;
+
+    const swiper = new Swiper(".swiper", {
+      autoplay: {
+        delay: 0,
+        pauseOnMouseEnter: true, 
+        disableOnInteraction: false, 
+        reverseDirection: true, 
+      },
+      slidesPerView: 4, 
+      speed: 3000,
+      slidesPerView: 'auto',
+	  centerInsufficientSlides:true,
+
+      scrollbar: {
+        el: ".swiper-scrollbar",
+      },
+    });
+
+    img_box.append(img, title, release_date);
+    place.append(img_box);
+  }
+}
+
+export function reload_top_rated(arr, place) {
 	place.innerHTML = ""
 	for(let item of arr) {
-		let img_box = document.createElement('div')
-		let img = document.createElement('img')
-		let img_title = document.createElement('h2')
-		let origin_title = document.createElement('p')
-		let known_for = document.createElement('span')
-		let div = document.createElement('div')
-
 		console.log(item);
+		let img_box = document.createElement('div');
+		let img = document.createElement('img');
+		let img_title = document.createElement('h2');
+		let img_vote_average = document.createElement('span');
+		let img_release_date = document.createElement('p');
+		let img_info_box = document.createElement('div');
 
-		img_box.classList.add('pop_stars_box')
-		img.classList.add('pop_stars_img')
-		div.classList.add('pop_stars_div')
 
-		img.src = 'https://image.tmdb.org/t/p/original' + item.profile_path
-		img_title.innerHTML = item.name
-		origin_title.innerHTML = item.original_name
-		div.append(img_title, origin_title, known_for)
-		img_box.append(img, div)
+		img.src = "https://image.tmdb.org/t/p/original" + item.poster_path
+		img_title.innerHTML = item.title
+		img_vote_average.innerHTML = item.vote_average
+		img_release_date.innerHTML = item.release_date
+
 		place.append(img_box)
-
-		console.log(item);
-
-		for(let film of item.known_for) {
-			// console.log(film);
-			known_for.innerHTML = film.title
-		}
+		img_box.append(img, img_info_box)
+		img_info_box.append(img_title, img_vote_average, img_release_date)
 	}
 }
