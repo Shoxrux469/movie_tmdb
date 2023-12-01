a7cad344ef4f2fd7caf24111cb4b5db68c414143
Шохрух <138532283+Shoxrux469@users.noreply.github.com>
2023-12-01 16:25:27 +0500

    fixed

--- pages/movies/script.js
+++ pages/movies/script.js
@@ -1,3 +1,9 @@
-import { header } from "../../modules/ui.js";
+import { header } from "../../modules/ui";
+import { getData } from "../../modules/https";
+// header();
+let id = location.search.split('=').at(-1)
+console.log(id);
 
-header();
+getData(`/movie/${id}`)
+    .then(res => console.log(res))
+// document.querySelector('.poster_img').src = "https://image.tmdb.org/t/p/original"
